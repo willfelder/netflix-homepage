90a2ff17c294b4fafa058326ec03aed868798eb6
William Lengenfelder <willfelder@users.noreply.github.com>
2021-04-03 22:00:09 -0300

    update project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { useEffect, useState } from 'react';
 import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState(null);
 
   useEffect (() => {
     const loadAll = async () => {
        
+      // Taking the total list
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Taking the featured 
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
     }
     loadAll();
   }, []);
 
   return (
     <div className="page">
+      {featuredData &&
+      <FeaturedMovie item={featuredData} />
+      }
+
+
        <section className="lists">
          {movieList.map((item, key) => (
            <MovieRow key={key} title={item.title} items={item.items} />
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -51,5 +51,21 @@ export default {
                 items: await basicFetch(`/discover/movie?with_genres=99&language=en&api_key=${API_KEY}`)
             }
         ]
-    }
+    },
+
+         getMovieInfo: async (movieId, type) => {
+             let info = {};
+
+               if(movieId){
+                   switch(type){
+                       case 'movie':
+                           info = await basicFetch(`/movie/${movieId}?language=en&api_key${API_KEY}`);
+                       break;
+
+                       case 'tv':
+                           info = await basicFetch(`/tv/${movieId}?language=en&api_key${API_KEY}`);
+                       break;
+                   }
+               }
+         }
 }
