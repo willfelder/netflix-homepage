a37761fe4bdc1ad2a90983c766a4745d84b5008b
William Lengenfelder <willfelder@users.noreply.github.com>
2021-04-06 10:54:19 -0300

    update project

--- src/App.js
+++ src/App.js
@@ -62,9 +62,15 @@ export default () => {
        </section>
 
        <footer>
-         This Project was create for purposes of studying, all rights reserved by Netflix&copy;
+         This Project was create for purposes of studying, all rights reserved by Netflix&copy;<br />
          All data movies were taken from Themoviedb.org
        </footer>
+
+       {movieList.length <= 0 &&
+          <div className="loading">
+             <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" alt="Loading" />
+          </div>
+       }
 
     </div>
   );
